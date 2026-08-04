1e4da3f1aa8b7d6b464d35d9ae30add66bcd7c99
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-08-04 13:32:04 -0500

    ejercicio 1: crear variable n, para que pida datos a guardar del usuario

---
+++ 3482404-santiago-alvarado-mejia/src/ejercicio_1.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class ejercicio_1 {
+    public static void main(String[] args) {
+
+        int n= Integer.parseInt(JOptionPane.showInputDialog("cuantos numeros va ingresar"));
+        int [] numeros= new int[n];
+        int pares=0;
+        int impares=0;
+
+
+    }
+}
