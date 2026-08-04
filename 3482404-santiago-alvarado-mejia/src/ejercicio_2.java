d6b6bfbc7f3f16e6e81378f6741f7f87e8ade918
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-08-04 14:46:33 -0500

    ejercicio 2: se modifico los espaciados para mayor comodidad visual

--- 3482404-santiago-alvarado-mejia/src/ejercicio_2.java
+++ 3482404-santiago-alvarado-mejia/src/ejercicio_2.java
@@ -1,2 +1,23 @@
+import javax.swing.*;
+
 public class ejercicio_2 {
+    public static void main(String[] args) {
+
+        int n= Integer.parseInt(JOptionPane.showInputDialog("cuantos estudiantes hay"));
+        double [] notas= new double[n];
+        double suma= 0;
+        double promedio;
+
+        for (int i=0; i<n;++i){
+           notas[i]=Double.parseDouble(JOptionPane.showInputDialog("ingrese la nota del estudiante " + (i+1) + " (0.0 a 5.0)"));
+           suma += notas[i];
+        }
+        promedio= suma/n;
+        if (promedio>3.0){
+            JOptionPane.showMessageDialog(null,"promedio del curso " + promedio + "\nEstado Reprobado");
+        }else {
+            JOptionPane.showMessageDialog(null,"Promedio del curso " + promedio + "\nEstado Aprobado");
+        } }else if (promedio !=1){
+        JOptionPane.showMessageDialog(null,"Promedio del curso " + promedio + "\nEstado Aprobado");
+    }
 }
