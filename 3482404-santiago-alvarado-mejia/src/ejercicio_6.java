8844509b8712e64e42a997501d14507f53a5e0e2
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-08-06 14:16:09 -0500

    ejercicio 6: se usaron mas variables como resultado, para utilizar en las validaciones

--- 3482404-santiago-alvarado-mejia/src/ejercicio_6.java
+++ 3482404-santiago-alvarado-mejia/src/ejercicio_6.java
@@ -14,5 +14,34 @@ public class ejercicio_6 {
             votos[i] = JOptionPane.showInputDialog("Ingrese el voto " + (i + 1) + " (A, B o C)").toUpperCase();
         }
 
+        for (int i=0;i< votos.length;i++){
+            if (votos[i].equals("A")){
+                votosA++;
+            }else if (votos[i].equals("B")){
+                votosB++;
+            }else if (votos[i].equals("C")){
+                votosC++;
+            }
+        }
+
+        String resultado="";
+
+        resultado += "candidato A: " + votosA + "\n";
+        resultado += "candidato B: " + votosB + "\n";
+        resultado += "candidato C: " + votosC + "\n";
+
+
+        if (votosA> votosB && votosA > votosC) {
+            resultado += "Ganador: A";
+        } else if (votosB > votosA && votosB > votosC) {
+            resultado += "Ganador: B";
+        } else if (votosC > votosA && votosC > votosB) {
+            resultado += "Ganador: C";
+        } else {
+            resultado += "Hay un empate.";
+        }
+
+        JOptionPane.showMessageDialog(null, resultado);
+
     }
 }
