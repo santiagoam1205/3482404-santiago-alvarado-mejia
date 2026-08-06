056a54de6b88cec5260dec71b30f4094a81ff9cc
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-08-06 13:00:27 -0500

    ejercicio 4:  se utilizo un ciclo for el cual sirve para agregar o guardar las temperaturas y otro for para clasificarlas

--- 3482404-santiago-alvarado-mejia/src/ejercicio_4.java
+++ 3482404-santiago-alvarado-mejia/src/ejercicio_4.java
@@ -1,6 +1,26 @@
+import javax.swing.*;
+
 public class ejercicio_4 {
     public static void main(String[] args) {
 
+        int n= Integer.parseInt(JOptionPane.showInputDialog("¿Cuantas temperaturas vas a registrar"));
+        int [] temperaturas= new int[n];
 
+        for (int i=0; i<n; ++i){
+            temperaturas [i]= Integer.parseInt(JOptionPane.showInputDialog("ingrese la temperatura " + (i + 1) + ": "));
+
+        }
+
+        String resultado="";
+
+        for (int i=0; i < temperaturas.length; i++){
+            if (temperaturas[i]<10){
+                resultado += temperaturas [i] + "°C - frio\n";
+            } else if (temperaturas[i] <= 25){
+                resultado += temperaturas[i] + "°C - templado\n";
+            }else {
+                resultado += temperaturas[i] + "°C - templado\n";
+            }
+        }
     }
 }
