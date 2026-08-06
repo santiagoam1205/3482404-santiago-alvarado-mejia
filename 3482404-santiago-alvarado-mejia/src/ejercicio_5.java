78e5d2715a16449343e21b0b8f8ef77f8053ed00
santiago alvarado mejia <santiagoam12005@gmail.com>
2026-08-06 13:36:05 -0500

    ejercicio 5: se implemento y mejoro el segundo for el cual va permitir buscar el numero seleccionado por el usuario, se cambiaron condicionales y comparaciones en estos

--- 3482404-santiago-alvarado-mejia/src/ejercicio_5.java
+++ 3482404-santiago-alvarado-mejia/src/ejercicio_5.java
@@ -9,5 +9,22 @@ public class ejercicio_5 {
         for (int i= 0; i < numeros.length; i++){
             numeros[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingrese numero" + (i + 1) + ": "));
         }
+        int buscar = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero que desea buscar: "));
+
+        boolean encontrado = false;
+        String mensaje ="";
+
+        for (int i = 0; i< numeros.length; i++) {
+            if (numeros[i] == buscar) {
+                encontrado = true;
+                mensaje += "Encontrado en la posicion: " + i + "\n";
+            }
+
+        }
+        if (encontrado){
+            JOptionPane.showMessageDialog(null,mensaje);
+        }else {
+            JOptionPane.showMessageDialog(null,"el numero no fue encontrado.");
+        }
     }
 }
